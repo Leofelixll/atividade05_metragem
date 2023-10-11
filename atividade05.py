81c62e3b98369224ea06540138c985ab97e48511
Leofelixll <fleo16leo@gmail.com>
2023-10-11 11:45:08 -0300

    first commit

---
+++ atividade05.py
@@ -0,0 +1,8 @@
+# Digite uma metragem e veja isso em cm e mm:
+
+print("conversão de milimetro para centimetro")
+metros = float(input("digite o valor em metros: "))
+centimetro = metros * 100
+milimetro = metros * 10
+print(f"seu valor em centimetro é: {centimetro}")  
+print(f"seu valor em milimetro é : {milimetro}")
